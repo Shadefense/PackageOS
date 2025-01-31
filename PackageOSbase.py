23b8e6ac1dfd9a0eff95875a35ad0eb51d887069
Shadefense <31imecham@go.dsdmail.net>
2025-01-31 21:32:48 +0000

    fix progress bar

--- PackageOSbase.py
+++ PackageOSbase.py
@@ -1,40 +1,39 @@
 import sys, time, random, os
 from time import sleep
 
-def progressBar(count_value, total, suffix=''):
-        bar_length = 100
-        filled_up_Length = int(round(bar_length* count_value / float(total)))
-        percentage = round(100.0 * count_value/float(total),1)
-        bar = '=' * filled_up_Length + '-' * (bar_length - filled_up_Length)
-        sys.stdout.write('[%s] %s%s %s\r' %(bar, percentage, '%', suffix))
+class PackageOSBase:
+    def __init__(self):
+        pass
+
+    def progress_bar(self, count_value, total, suffix=''):
+        bar_length = 50  # Adjusted for better visibility
+        filled_up_length = int(round(bar_length * count_value / float(total)))
+        percentage = round(100.0 * count_value / float(total), 1)
+        bar = '=' * filled_up_length + '-' * (bar_length - filled_up_length)
+        sys.stdout.write('\r[%s] %s%s %s' % (bar, percentage, '%', suffix))
         sys.stdout.flush()
-def loadingScreen(logFile):
-    screen = """
+
+    def loading_screen(self, log_file):
+        screen = """
                     ██████╗  █████╗  ██████╗██╗  ██╗ █████╗  ██████╗ ███████╗
                     ██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██╔══██╗██╔════╝ ██╔════╝
                     ██████╔╝███████║██║     █████╔╝ ███████║██║  ███╗█████╗  
                     ██╔═══╝ ██╔══██║██║     ██╔═██╗ ██╔══██║██║   ██║██╔══╝  
                     ██║     ██║  ██║╚██████╗██║  ██╗██║  ██║╚██████╔╝███████╗
-                    ╚═╝     ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝
-    """
-    print(screen)
-    shell = os.environ['SHELL']
-    for i in range(11):
-        time.sleep(random.random()) #task to do
-        progressBar(i,10, "loaded") 
-    sleep(500)
-    if shell == "/bin/bash":
-        os.system("clear")
-    else:
-        os.system("cls")
+                    ╚═╝     ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═════╝ ╚══════╝
+        """
+        print(screen)
+        for i in range(11):
+            time.sleep(random.random())  # Simulate task
+            self.progress_bar(i, 10, "loaded")
+        print()  # Move to the next line after the progress bar is complete
+        sleep(1)  # Short sleep to allow the user to see the completed progress bar
+        os.system('clear' if os.name == 'posix' else 'cls')
 
-
-
-        
-loadingScreen(None)
-
-print(os.environ['SHELL'])
-        
+# Example usage
+if __name__ == "__main__":
+    package_os_base = PackageOSBase()
+    package_os_base.loading_screen("logfile.txt")
 
 
 
